157f07b686a566a14fc8dc2cc6ed5b33cdbe50b1
Vagan Pogosyan <vagan2502@gmail.com>
2023-10-28 12:10:47 -0400

    add login route

---
+++ modules/users/controllers/loginUser.js
@@ -0,0 +1,29 @@
+const validateEmail = require("../../../utils/validateEmail");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const loginUser = async (req, res) => {
+  const userModel = mongoose.model("user");
+
+  const { email, password } = req.body;
+
+  const foundUser = await userModel.findOne({
+    email: email,
+  });
+
+  if (!email) throw "Email is required";
+  if (!validateEmail(email)) throw "Invalid email";
+  if (!foundUser) throw "User with this email doesn't exist";
+  if (!password) throw "Password is required";
+
+  const comparePassword = await bcrypt.compare(password, foundUser.password);
+  if (!comparePassword) throw "Email and password do not match";
+
+  res.status(200).json({
+    status: "success",
+    message: "Logged in successfully!",
+    data: foundUser,
+  });
+};
+
+module.exports = loginUser;
--- modules/users/user.routes.js
+++ modules/users/user.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const registerUser = require("./controllers/registerUser");
+const loginUser = require("./controllers/loginUser");
 
 // Set express Router for users
 const userRouter = express.Router();
 
 // Routes
 userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
 
 module.exports = userRouter;
