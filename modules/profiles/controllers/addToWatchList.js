5c1b787d1c22dd85170c6bbb4854f2c44f052230
Vagan Pogosyan <vagan2502@gmail.com>
2023-10-29 19:22:20 -0400

    add to watchlist and remove if movie already added

--- modules/profiles/controllers/addToWatchList.js
+++ modules/profiles/controllers/addToWatchList.js
@@ -1,17 +1,36 @@
 const mongoose = require("mongoose");
 
 const addToWatchList = async (req, res) => {
-  console.log(req.params);
   const profileModel = mongoose.model("profile");
 
-  await profileModel.updateOne(
-    { _id: req.params.profile_id },
-    {
-      $push: { watch_list: { movie_id: req.params.movie_id } },
-    },
+  // check if movie already added to watchlist
+  const { watch_list } = await profileModel
+    .findById(req.params.profile_id)
+    .select("watch_list");
 
-    { runValidators: true }
+  const foundMovieIndex = watch_list.findIndex(
+    (movie) => movie.movie_id === req.params.movie_id
   );
+
+  if (foundMovieIndex > -1) {
+    watch_list.splice(foundMovieIndex, 1);
+    await profileModel.updateOne(
+      { _id: req.params.profile_id },
+      {
+        $set: { watch_list: watch_list },
+      },
+      { runValidators: true }
+    );
+  } else {
+    await profileModel.updateOne(
+      { _id: req.params.profile_id },
+      {
+        $push: { watch_list: { movie_id: req.params.movie_id } },
+      },
+
+      { runValidators: true }
+    );
+  }
 
   res.status(200).json({
     status: "success",
